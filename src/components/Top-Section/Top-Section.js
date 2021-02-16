8ddb4ff33f50b8c75c6ebb16a6430e27bbed46b7
Poujhit <poujhit.dev@gmail.com>
2021-02-16 13:49:00 +0530

    right section of top over

--- src/components/Top-Section/Top-Section.js
+++ src/components/Top-Section/Top-Section.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import { Navbar } from './Navbar';
 import Image1 from '../../images/image1.png';
+import Image2 from '../../images/image2.png';
+import Image3 from '../../images/image3.png';
+import Image4 from '../../images/image4.png';
+import Image5 from '../../images/image5.png';
 
 import './Top-Section.css';
 
@@ -10,17 +14,24 @@ function TopSection() {
     <div className='Top-Section'>
       <Navbar />
       <img src={Image1} className='Image-1' />
-      <div className='Top-section-content-right'>
+      <div className='Top-section-content-left'>
         <text className='Intro'>Introducing</text>
         <br />
         <text className='Intro2'>Our latest VR headset</text>
         <br />
         <br />
-        <text className = "Intro3" >
-          UNTETHER YOUR EXPECTATIONS EXPLORE AWE-INSPIRING GAMES AND EXPERIENCES WITH UNPARALLELED FREEDOM.
+        <text className='Intro3'>
+          UNTETHER YOUR EXPECTATIONS EXPLORE AWE-INSPIRING GAMES AND EXPERIENCES
+          WITH UNPARALLELED FREEDOM.
         </text>
       </div>
-      <button className = "Intro-button" >Buy Now</button>
+      <button className='Intro-button'>Buy Now</button>
+      <div className='Top-section-content-right'>
+        <img src={Image2} className='Image-2' />
+        <img src={Image3} className='Image-3' />
+        <img src={Image4} className='Image-4' />
+        <img src={Image5} className='Image-5' />
+      </div>
     </div>
   );
 }
